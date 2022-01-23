1b6147c0205e55b0006f103aa9169904724a76ce
Vutikuri <Anil.kumar.Vutikuri@Dell.com>
2022-01-23 16:02:44 +0530

    first commit

---
+++ src/main/java/com/javatechie/azure/demon/SpringAzureDemonApplication.java
@@ -0,0 +1,20 @@
+package com.javatechie.azure.demon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemonApplication {
+
+    @GetMapping("/message")
+    public String message(){
+    return "Congrats ! Your app deployed in Azure";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAzureDemonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatechie/azure/demon/SpringAzureDemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.azure.demon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
